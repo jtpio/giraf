a90852bc298f361fb734826211ed51bfaf6d69dd
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2015-02-07 10:18:30 -0800

    Bounce the giraffe a little bit
    Also imported the default skyboxes

---
+++ Giraf/Assets/Giraf/Scripts/Bounce.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+	
+	public Vector3 direction;
+	public float amplitude;
+	public float speed;
+	
+	float phase;
+
+	void Start () {
+		if (direction.magnitude == 0) {
+			direction = new Vector3(0.0f, 1.0f, 0.0f);
+		}
+		if (speed == 0) {
+			speed = 1;
+		}
+		phase = Random.Range(0.0f, Mathf.PI);
+	}
+	
+	void Update () {
+		transform.position = transform.position + direction * amplitude * Mathf.Sin(speed * Time.time + phase);
+	}
+}
