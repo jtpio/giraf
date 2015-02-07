d5c81450ecee6cfeb436892c1629eae0b9b8fa69
Dharam Kapila <dharamkapila@gmail.com>
2015-02-07 19:16:31 +0100

    Refactor obstruction object to have states.

--- Giraf/Assets/Map/Scripts/Obstruction.cs
+++ Giraf/Assets/Map/Scripts/Obstruction.cs
@@ -7,10 +7,11 @@ public class Obstruction : MonoBehaviour {
 	private float currentLifeTime = 0.0f;
     private Vector3 startingPosition;
     private float currentOffsetY;
-	private bool explode = false;
 	private bool hidden = false;
 	private string mineName = "Mine";
 	private string sphereName = "Sphere001";
+	private ExplodeParticle particle;
+	private ObstructionState state = ObstructionState.Falling;
 
 	public float totalLifeTime = 5;
     public float startingOffsetY = 600.0f;
@@ -27,38 +28,73 @@ public class Obstruction : MonoBehaviour {
 	void Update () {
 		this.currentLifeTime += Time.deltaTime;
 		print (this.currentLifeTime);
+		this.SetState();
+		this.RenderState();
+	}
 
-		if (this.currentLifeTime + 0.2 >= this.totalLifeTime && !this.explode) {
-			this.explode = true;
+	private void SetState () {
+		if (this.currentLifeTime + 0.2 >= this.totalLifeTime && this.state == ObstructionState.Falling) {
+			this.state = ObstructionState.Exploding;
+		}
+		
+		if(this.currentLifeTime >= this.totalLifeTime && this.state == ObstructionState.Dying) {
+			this.state = ObstructionState.Dead;
+		} 
+	}
 
-			var explosionParticle = Instantiate(this.explosionParticle) as Transform;
-			explosionParticle.position = (transform.Find(this.mineName)).position;
-			this.audio.Play ();
+	private bool RenderState () {
+		switch (this.state) {
+			case ObstructionState.Falling : {		
+				if (this.currentOffsetY > 0.0f) {
+					this.currentOffsetY -= Time.deltaTime * 100.0f;
+					
+					this.UpdatePosition();
+				} else if (this.currentOffsetY < 0.0f) {
+					this.currentOffsetY = 0.0f;
+					this.UpdatePosition();
+				}
+
+				break;
+			}
+
+			case ObstructionState.Exploding : {
+			print ("Exploding");
+				this.explosionParticle = Instantiate(this.explosionParticle) as Transform;
+				explosionParticle.position = (transform.Find(this.mineName)).position;
+
+				if (!audio.isPlaying) {
+					this.audio.Play ();
+				}
+
+			    this.state = ObstructionState.Dying;
+
+				break;
+			}
+
+			case ObstructionState.Dying : {
+				break;
+			}
+
+			case ObstructionState.Dead : {
+				this.areaMapper.hasObstruction = false;
+				if (!this.hidden) {
+					this.hidden = true;
+					(transform.Find(this.mineName) as Transform).GetComponent<MeshRenderer>().enabled = false;
+					(transform.Find(this.sphereName) as Transform).GetComponent<MeshRenderer>().enabled = false;
+					
+				} else if (!audio.isPlaying) {
+					Destroy(gameObject);
+				}
+
+				break;
+			}
+
+			default:  {
+				return false;
+			}
 		}
 
-        if(this.currentLifeTime >= this.totalLifeTime) {
-			this.areaMapper.hasObstruction = false;
-
-			if (!this.hidden) {
-				this.hidden = true;
-				(transform.Find(this.mineName) as Transform).GetComponent<MeshRenderer>().enabled = false;
-				(transform.Find(this.sphereName) as Transform).GetComponent<MeshRenderer>().enabled = false;
-
-			} else if (!audio.isPlaying) {
-				Destroy(gameObject);
-			}
-		} else {
-
-            if (this.currentOffsetY > 0.0f) {
-                this.currentOffsetY -= Time.deltaTime * 100.0f;
-
-                this.UpdatePosition();
-            } else if (this.currentOffsetY < 0.0f) {
-                this.currentOffsetY = 0.0f;
-                this.UpdatePosition();
-            }
-
-        }
+		return true;
 	}
 
     void UpdatePosition() {
---
+++ Giraf/Assets/Map/Scripts/ObstructionState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public enum ObstructionState {
+	Falling,
+	Exploding,
+	Dying,
+	Dead
+}
